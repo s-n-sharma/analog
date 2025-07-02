703cf43dcd086310cc59c0262e8b7c62dc4590e4
nandandiwan <ndiwan@berkeley.edu>
2025-07-01 22:24:01 -0700

    transformers and read me a lot has been done

--- src/transformer/transformer.py
+++
@@ -1,7 +0,0 @@
-import torch
-import torch.nn as nn
-
-class CircuitTransformer(nn.Module):
-    def __init__(self, src_vocab_size, tgt_vocab_size, d_model, nhead, num_encoder_layers, num_decoder_layers):
-        super(CircuitTransformer, self).__init__()  
-        self.d_model = d_model
